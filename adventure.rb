e15c940da4357d84884b915751bf4b42e0721272
Keith Pitt <me@keithpitt.com>
2016-07-10 12:37:30 +0930

    Redirect page and commands to different streams

--- adventure.rb
+++ adventure.rb
@@ -3,12 +3,10 @@
 require 'yaml'
 
 class Step
-  attr_reader :id, :script, :label, :actions
+  attr_reader :page, :actions
 
-  def initialize(id, script, label, actions)
-    @id = id
-    @script = script
-    @label = label
+  def initialize(page, actions)
+    @page = page
     @actions = actions
   end
 end
@@ -17,19 +15,19 @@ def to_buildkite_pipeline(step)
   {
     "steps" => [
       {
-        "command" => "ruby adventure.rb | buildkite-agent pipeline upload",
-        "label" => step.label,
-        "env" => { "ADVENTURE_SCRIPT" => step.script }
-      },
-      {
         "block" => ":point_right:",
         "prompt" => "Choose the next action!",
-        "fields" => [ { "text" => "Yes", "key" => "#{step.id}-action" } ]
+        "fields" => [ { "text" => "Yes", "key" => "decision" } ]
+      },
+      {
+        "command" => "ruby adventure.rb 2> >(buildkite-agent pipeline upload)",
+        "label" => ":book:"
       }
     ]
   }
 end
 
-s = Step.new(:yes, :first, ":llama::", [ "Go Left", "Go Right" ])
+s = Step.new("welcome", [ "Go Left", "Go Right" ])
 
-puts to_buildkite_pipeline(s).to_yaml
+STDOUT.puts `./pages/#{s.page}.sh`
+STDERR.puts to_buildkite_pipeline(s).to_yaml
