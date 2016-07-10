eb86774365f91541f409de5cc7ad873a2d5594d3
Keith Pitt <me@keithpitt.com>
2016-07-10 19:40:51 +0930

    Turn around words

--- pages/turn-around/render.sh
+++ pages/turn-around/render.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "To your left is the Chapel. There's a ton of people in there, something fun must be happening!"
+echo "To your right is a fire, looks like Nigel might be going there!"
+echo ""
+
 ./image "$(dirname $0)/chapel-or-fire.jpg"
