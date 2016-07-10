61007e3086bba182fb51f963b22075625611fcf4
Keith Pitt <me@keithpitt.com>
2016-07-10 12:24:24 +0930

    Started structuring the adventure

---
+++ adventure.rb
@@ -0,0 +1,33 @@
+require 'yaml'
+
+class Step
+  attr_reader :id, :script, :label, :actions
+
+  def initialize(id, script, label, actions)
+    @id = id
+    @script = script
+    @label = label
+    @actions = actions
+  end
+end
+
+def to_buildkite_pipeline(step)
+  {
+    "steps" => [
+      {
+        "command" => "adventure.rb",
+        "label" => step.label,
+        "env" => { "ADVENTURE_SCRIPT" => step.script }
+      },
+      {
+        "block" => ":point_right:",
+        "prompt" => "Choose the next action!",
+        "fields" => [ { "text" => "Yes", "key" => "#{step.id}-action" } ]
+      }
+    ]
+  }
+end
+
+s = Step.new(:yes, :first, ":llama::", [ "Go Left", "Go Right" ])
+
+puts to_buildkite_pipeline(s).to_yaml
